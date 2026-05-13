209f744bb9fae1f8eff3b7724c248545814bd810
Heamall <Heamall.anees@student.manchester.ac.uk>
2026-05-13 22:00:36 +0100

    Add files via upload

---
+++ Comaprison plot Cr + Al .r
@@ -0,0 +1,85 @@
+install.packages("ggrepel")
+
+library(ggplot2)
+library(ggrepel)
+
+# Sample data - Al2O3 (wt%) and Cr (ppm)
+data <- data.frame(
+  sample = c("GK-2", "GK-3", "HH-1", "HH-2", "HH-3", 
+             "KK-1", "KK-2", "KKF-1", "KKF-2", "KKF-3", "KKF-4", "KKF-5"),
+  group = c("GK", "GK", "HH", "HH", "HH", 
+            "KK", "KK", "KKF", "KKF", "KKF", "KKF", "KKF"),
+  Al2O3 = c(5.2422, 6.3423, 9.1841, 4.9039, 7.0781,
+            2.4449, 4.8967, 8.4659, 4.8450, 3.3062, 8.1666, 10.0037),
+  Cr_ppm = c(204.0, 339.0, 165.0, 95.0, 246.0,
+             116.0, 281.0, 119.0, 252.0, 128.0, 255.0, 1204.0)
+)
+
+# PAAS reference point
+paas <- data.frame(
+  sample = "PAAS",
+  Al2O3  = 18.9,
+  Cr_ppm = 110
+)
+
+# Group colours
+group_colours <- c(
+  "GK"  = "#D85A30",
+  "HH"  = "#378ADD",
+  "KK"  = "#534AB7",
+  "KKF" = "#1D9E75"
+)
+
+ggplot() +
+  # Sample points
+  geom_point(data = data,
+             aes(x = Al2O3, y = Cr_ppm, colour = group),
+             size = 4, shape = 16) +
+  # Repelled sample labels
+  geom_text_repel(data = data,
+                  aes(x = Al2O3, y = Cr_ppm, label = sample, colour = group),
+                  size = 3.2,
+                  box.padding = 0.4,
+                  point.padding = 0.3,
+                  segment.size = 0.3,
+                  segment.colour = "grey60",
+                  max.overlaps = Inf,
+                  show.legend = FALSE) +
+  # PAAS star
+  geom_point(data = paas,
+             aes(x = Al2O3, y = Cr_ppm),
+             shape = 8, size = 4, colour = "#E8B000", stroke = 1.0) +
+  geom_text_repel(data = paas,
+                  aes(x = Al2O3, y = Cr_ppm, label = sample),
+                  size = 3.2,
+                  colour = "#E8B000",
+                  fontface = "plain",
+                  box.padding = 0.5,
+                  point.padding = 0.3,
+                  segment.size = 0.3,
+                  segment.colour = "grey60",
+                  show.legend = FALSE) +
+  # Scales and labels
+  scale_colour_manual(values = group_colours, name = "Sample Group") +
+  scale_x_continuous(limits = c(0, 22),
+                     breaks = seq(0, 22, 2),
+                     labels = function(x) paste0(x, "%")) +
+  scale_y_continuous(limits = c(0, 1300),
+                     breaks = seq(0, 1300, 200)) +
+  labs(
+    x       = expression(Al[2]*O[3]~"(wt%)"),
+    y       = "Cr (ppm)",
+    title   = expression(Al[2]*O[3]~"vs Cr"),
+    caption = "PAAS values from Taylor and McLennan (1985)"
+  ) +
+  theme_bw() +
+  theme(
+    plot.title       = element_text(hjust = 0.5, size = 13, face = "bold"),
+    axis.title       = element_text(size = 11),
+    axis.text        = element_text(size = 10),
+    legend.title     = element_text(size = 10, face = "bold"),
+    legend.text      = element_text(size = 9),
+    legend.position  = "right",
+    panel.grid.minor = element_blank(),
+    plot.caption     = element_text(size = 8, colour = "grey50", hjust = 0)
+  )
